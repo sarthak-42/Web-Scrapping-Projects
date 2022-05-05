5bb2bfeea24a55205da1e9109d9afe948096432b
sarthak-42 <sarthakjaiswal4200@gmail.com>
2022-05-06 02:11:17 +0530

    Scrapping

---
+++ coronadetails.js
@@ -0,0 +1,39 @@
+const request = require ('request')
+const cheerio = require ('cheerio')
+
+
+console.log('Before')
+
+request('https://www.worldometers.info/coronavirus/' ,cb)
+function cb (error, response, html){
+    if(error){
+        console.error(error);
+    }
+    else{
+        handleHtml(html);
+    }
+}
+
+
+function handleHtml(html){
+    let selector = cheerio.load(html)
+    let contentArr = selector( '.maincounter-number span')
+
+
+    // multiple same classes ko broser as an array store krta hai that's why hum conent me array ki properties ka use kr skte hai
+
+
+
+
+    let totalCases = selector(contentArr[0]).text() //.text cheerio ka funtion hai jo data ko simple text me convert krta hai
+    console.log('Total Cases : ' + totalCases)
+    
+    let totalDeaths = selector(contentArr[1]).text()
+    console.log('Total Deaths : ' + totalDeaths)
+    
+    let Recovery = selector(contentArr[2]).text()
+    console.log('Recovery : ' + Recovery)
+}
+
+
+console.log('after')
